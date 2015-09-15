60b807fb222c9e33f98748d97080ffb98d0f7a66
Joshua White <joshua.jj.white@gmail.com>
2015-09-14 18:29:02 -0600

    "Added new classes"

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -23,10 +23,17 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayList<Tweet> tweetList;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Tweet tweet;
+
+		try { tweet = new ImportantTweet("longer than 140 characters");}
+		catch (IllegalArgumentException exception){
+			// do somthing here
+		}
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
